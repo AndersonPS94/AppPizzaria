554c31d3542433e9aa543fe5b7a15a84a83ee80c
AndersonPS94 <dson_adn@hotmail.com>
2024-01-15 02:32:04 -0300

    app de pizzaria em desenvolvimento 2

--- app/src/main/java/com/example/appdepizzaria/MainActivity.kt
+++ app/src/main/java/com/example/appdepizzaria/MainActivity.kt
@@ -4,8 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var clicked = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btAll.setOnClickListener {
+            clicked = true
+            if (clicked){
+                binding.btAll.setBackgoundResource(R.drawable.bg_button_enabled)
+                binding.btAll.setTextColor(Color.WHITE)
+                binding.btnChicken.setBackgroundResource(R.drawable.bg_button_disabled)
+            }
+        }
     }
 }
